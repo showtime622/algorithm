cb715ef394fd84ba763a098daecad5ee0d3ed0de
superliu <superliu622@live.cn>
2018-11-27 22:23:04 +0800

    sort array II

--- AlgorithmConsole/SortArrayByParity.cs
+++ AlgorithmConsole/SortArrayByParity.cs
@@ -49,5 +49,85 @@ namespace AlgorithmConsole
             }
 
         }
+		
+		  public static int[] SortArrayByParityII(int[] A)
+        {
+            if(A.Length == 0)
+            {
+                return A;
+            }
+
+            int i = 0;
+            int j = A.Length - 1;
+            int tempj = j;
+
+            while(i < j)
+            {
+               if(A[i] % 2 == 0 && i % 2 != 0)
+               {
+                    if(A[j] %2 != 0 && j % 2 == 0)
+                    {
+                        int temp = A[i];
+                        A[i] = A[j];
+                        A[j] = temp;
+                        i++;
+                        j = A.Length - 1;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+               else if(A[i] % 2 != 0 && i % 2 == 0)
+               {
+                    if (A[j] % 2 == 0 && j % 2 != 0)
+                    {
+                        int temp = A[i];
+                        A[i] = A[j];
+                        A[j] = temp;
+                        i++;
+                        j = A.Length - 1;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+               else
+               {
+                    i++;
+               }
+            }
+
+            return A;
+        }
+        /// <summary>
+        /// very smart sol from others
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public static int[] SortArrayByParityIINew(int[] A)
+        {
+            int e = 0;
+            int o = 1;
+
+            while(o < A.Length && e < A.Length)
+            {
+                if(A[e] % 2 != 0)
+                {
+                    int temp = A[e];
+                    A[e] = A[o];
+                    A[o] = temp;
+                    o += 2;
+                }
+                else
+                {
+                    e += 2;
+                }
+            }
+
+            return A;
+        }
+    
     }
 }
